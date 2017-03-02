5a0a3e42fa692398d6e7de2c506bc9bdb7335082
arturparkhisenko <ikeagold@gmail.com>
2017-03-02 23:24:53 +0200

    added auto archiver

--- index.js
+++ index.js
@@ -56,6 +56,23 @@ let files = test ? ['data-examples/example.in'] : [
 
 // processes-end --------------------------------------------------------------
 
+// zip ------------------------------------------------------------------------
+const archiver = require('archiver');
+fs.unlink(__dirname + '/data-examples/code.zip', ()=>{});
+// create a file to stream archive data to.
+const output = fs.createWriteStream(__dirname + '/data-examples/code.zip');
+const archive = archiver('zip', {
+    store: true // Sets the compression method to STORE.
+});
+// pipe archive data to the file
+archive.pipe(output);
+// append a files from stream
+archive.append(fs.createReadStream(__dirname + '/index.js'), { name: 'index.js' });
+archive.append(fs.createReadStream(__dirname + '/package.json'), { name: 'package.json' });
+// finalize the archive (ie we are done appending files but streams have to finish yet)
+archive.finalize();
+// zip-end --------------------------------------------------------------------
+
 let filesPromises = [];
 for (const fileName of files) {
   filesPromises.push(processFile(fileName));
